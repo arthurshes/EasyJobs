d8f564fdc6875274ab3df4c86ba2de51673dd077
Radion <radion20077@gmail.com>
2023-03-03 16:42:29 +0600

    add bottomView and view binding

--- EasyJobs/app/src/main/java/com/example/easyjobs/Splash/activities/MainContentActivity.kt
+++ EasyJobs/app/src/main/java/com/example/easyjobs/Splash/activities/MainContentActivity.kt
@@ -13,30 +13,36 @@ class MainContentActivity : AppCompatActivity() {
         binding = ActivityMainContentBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setButtNavView()
+        binding.toolbar.title=getString(R.string.tinder_job)
     }
 
 
-    private fun setButtNavView() {
+    private fun setButtNavView() =with(binding){
         binding.bottomPanel.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.id_tinder_job -> {
                     showToast("tinder_job")
+                   toolbar.title=getString(R.string.tinder_job)
                     true
                 }
                 R.id.id_search_job -> {
                     showToast("search_job")
+                    toolbar.title=getString(R.string.search)
                     true
                 }
                 R.id.id_favorite -> {
                     showToast("favorite")
+                    toolbar.title=getString(R.string.favorite)
                     true
                 }
                 R.id.id_chat -> {
                     showToast("chat")
+                    toolbar.title=getString(R.string.chat)
                     true
                 }
                 R.id.id_account -> {
                     showToast("account")
+                    toolbar.title=getString(R.string.account)
                     true
                 }
                 else -> {
